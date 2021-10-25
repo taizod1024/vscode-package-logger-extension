b34349291ee779aa06d937e25d3e035f1991d317
yamamoto.taizo <taizod1024@gmail.com>
2021-10-25 20:38:40 +0900

    refactor: ファイル名変更

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { packagelogger } from './PackageLogger_';
+import { packagelogger } from './PackageLogger';
 
 // extension entrypoint
 export function activate(context: vscode.ExtensionContext) {
